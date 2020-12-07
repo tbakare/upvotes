26b962104b95f00e951daffdda79c3eb7ef3d7e3
Alienware <Alienware@192.168.2.39>
2020-12-07 02:26:51 -0330

    Initial check-in of the upvotes code

---
+++ upvotes/src/main/java/com/upvotes/UpvotesApplication.java
@@ -0,0 +1,13 @@
+package com.upvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpvotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpvotesApplication.class, args);
+	}
+
+}
