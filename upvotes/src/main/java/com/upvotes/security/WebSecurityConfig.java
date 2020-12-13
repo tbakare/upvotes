a1affdb73c9463f73a6aa5f55d8a5acee2a0c042
DESKTOP-EB2ROQD\Alienware <bakaretemiloluwa@gmail.com>
2020-12-13 14:04:00 -0330

    created csrf branch

--- upvotes/src/main/java/com/upvotes/security/WebSecurityConfig.java
+++ upvotes/src/main/java/com/upvotes/security/WebSecurityConfig.java
@@ -1,30 +1,39 @@
 package com.upvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.inMemoryAuthentication()
+			.passwordEncoder(getPasswordEncoder())
 			.withUser("temmy@yahoo.com")
-			.password("asdfasdf")
+			.password(getPasswordEncoder().encode("asdfasdf"))
 			.roles("USER");
 		
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.csrf().disable()
+		http
 			.authorizeRequests()
 			.antMatchers("/").permitAll()
 			.anyRequest().hasAnyRole("USER").and()
 			.formLogin()
-			.loginPage("/login").permitAll().and()
+			.loginPage("/login").defaultSuccessUrl("/dashboard").permitAll().and()
 			.logout()
 			.logoutUrl("/logout").permitAll();
 	}
